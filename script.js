25d53cf02e495a4ee10b0db3bc5c7c8ce211cc57
Hooda1465 <sunilmedbr@gmail.com>
2024-09-07 22:02:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,207 +1,158 @@
 
+// Element references
+const processingMessage = document.getElementById('processingMessage');
+const doneMessage = document.getElementById('doneMessage');
+const inputAdmin = document.getElementById("inputAdmin");
+const btnAdmin = document.getElementById("btnAdmin");
+const adminAccessBtn = document.getElementById("adminAccessBtn");
+const pTag = document.getElementById("pTag");
+const uploadBox = document.getElementById("uploadBox");
+const uploadData = document.getElementById("uploadData");
+const downloadData = document.getElementById("downloadData");
+const flexCheckDefault = document.getElementById("flexCheckDefault");
+const imgDiv = document.getElementById("imgDiv");
+const zipCode = document.getElementById("zipVal");
+const searchBtn = document.getElementById("searchZip");
+const table1 = document.getElementById("table1");
+const table2 = document.getElementById("table2");
+const hrTag = document.getElementById("hrTag");
+const table1Con = document.getElementById("table1Con");
+const table2Vio = document.getElementById("table2Vio");
+const errorMsg = document.getElementById("errorMsg");
 
+// Event listeners
+searchBtn.addEventListener("click", printVal);
 
+document.getElementById('processDataBtn').addEventListener('click', function() {
+    processingMessage.style.display = 'block';
+    processingMessage.textContent = 'Processing. Please Wait...'; // Ensure this text appears initially
 
-
-let inputAdmin=document.getElementById("inputAdmin");
-let btnAdmin=document.getElementById("btnAdmin");
-let adminAccessBtn=document.getElementById("adminAccessBtn");
-let pTag= document.getElementById("pTag");
-let uploadBox=document.getElementById("uploadBox");
-let uploadData=document.getElementById("uploadData");
-let downloadData=document.getElementById("downloadData");
-let flexCheckDefault=document.getElementById("flexCheckDefault");
-let imgDiv= document.getElementById("imgDiv")
-let zipCode = document.getElementById("zipVal");
-let SearchBtn= document.getElementById("searchZip");
-let table1 = document.getElementById("table1");
-let table2 = document.getElementById("table2");
-let hrTag = document.getElementById("hrTag");
-let table1Con = document.getElementById("table1Con");
-let table2Vio=document.getElementById("table2Vio");
-let errorMsg = document.getElementById("errorMsg");
-SearchBtn.addEventListener("click", printVal);
-
-function printVal(){
-   
-    let zipCodeVal=zipCode.value;
-
-// if(zipCodeVal && select==true){
-
-
-// }
-// if(zipCodeVal && select==false){
-
-
-// }
-
-    if(zipCodeVal==123){
-
-        let data=
-           [['PB90',
-             'LEAD SUMMARY',
-             'Lead is a toxic heavy metal that can be found in drinking water, primarily due to the corrosion of old plumbing systems, such as lead pipes, fixtures, and solder.',
-             'Lead exposure is highly dangerous, especially for children, as it can cause developmental delays, learning difficulties, and behavioral issues. In adults, lead can result in hypertension, kidney damage, and reproductive problems. Even low levels of lead in drinking water can have serious health effects, making it one of the most concerning contaminants.',
-             'LEAD & VOC Pack'],
+    fetch('/process-data')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.downloadLinks && Object.keys(data.downloadLinks).length > 0) {
+                let linksHTML = '<p>Data processed successfully! Here are your file links to download or access:</p>';                    
+                // Base path to remove
+                const basePathToRemove = 'root/Centrix-Engine-/';
             
-            ['CU90',
-             'COPPER SUMMARY',
-             'Copper is a metal that can leach into drinking water primarily from the corrosion of copper pipes and plumbing fixtures.',
-             'While copper is an essential nutrient in small amounts, excessive exposure can cause gastrointestinal distress, including nausea, vomiting, and diarrhea. Long-term exposure to high levels of copper in drinking water can lead to liver and kidney damage and may cause complications for individuals with Wilson’s disease, a genetic disorder that causes copper accumulation in the body.',
-             'Anti-Scale & Heavy Metals Pack'],
-            ['2V08',
-             '8 Regulated Phase I VOCs',
-             '\t1.\tBenzene\n\t2.\tCarbon Tetrachloride\n\t3.\t1,2-Dichloroethane\n\t4.\t1,1-Dichloroethylene\n\t5.\tTrichloroethylene\n\t6.\t1,1,1-Trichloroethane\n\t7.\tVinyl Chloride\n\t8.\tTetrachloroethylene (also known as Perchloroethylene)',
-             'This group of contaminants, which includes chemicals like Benzene and Vinyl Chloride, poses serious health risks including cancer, liver and kidney damage, and nervous system effects. Long-term exposure to these VOCs can lead to chronic health issues such as organ damage and increased cancer risk. Many of these chemicals are known carcinogens, making them particularly hazardous in drinking water.',
-             'Chloramine & VOC Pack'],
-            ['2V07',
-             '7 Regulated Phase I VOCs',
-             '\t1.\tBenzene\n\t2.\tCarbon Tetrachloride\n\t3.\t1,2-Dichloroethane\n\t4.\t1,1-Dichloroethylene\n\t5.\tTrichloroethylene\n\t6.\t1,1,1-Trichloroethane\n\t7.\tVinyl Chloride\n\t8.\tTetrachloroethylene (also known as Perchloroethylene)',
-             'This group of contaminants, which includes chemicals like Benzene and Vinyl Chloride, poses serious health risks including cancer, liver and kidney damage, and nervous system effects. Long-term exposure to these VOCs can lead to chronic health issues such as organ damage and increased cancer risk. Many of these chemicals are known carcinogens, making them particularly hazardous in drinking water.',
-             'Chloramine & VOC Pack']];
-        
-        
-        // [["CCC-125","abcd","5825852","This mear of rows","For example the array below hs three rows and four elements"],
-        // ["CCC-125","xyz","Skggh","the number of rows","For example whree rows and four elements"],
-        // ["CCC-126","RZ&RDT","PPPPPP","the number of rows","For example whree rows and four elements"],
-        // ["CCC-127","WRE"," two-dimensional","ay berows and four elements","arrays have a fix"],
-        // ["CCC-128","DD258"," note that ","This means that ","the array below has three rows and four elements"]
-        // ]
-    
-         
-            if(data.length>0){
-                table1Con.classList.remove("d-none");
-                hrTag.classList.remove("d-none");
-                let rows=""
-               
-                rows+= "<thead>";
-                rows+= "<tr>";
-                rows+= "<th>Code</th>";
-                rows+= "<th>Description</th>";
-                rows+= "<th>Contaminant Description</th>";
-                rows+= "<th>Health Hazard</th>";
-                rows+= "<th>Filter Pack Match</th>";
-                rows+= "</tr>";
-                rows+= "</thead>";
-                for(var i=0; i<data.length; i++){
-                  rows+= "<tbody>";
-                  rows+= "<tr>";
-                  rows+= "<td>"+ data[i][0] +"</td>";
-                  rows+= "<td>"+ data[i][1] +"</td>";
-                  rows+= "<td>"+ data[i][2] +"</td>";
-                  rows+= "<td>"+ data[i][3] +"</td>";
-                  rows+= "<td>"+ data[i][4] +"</td>";
-                  rows+= "</tr>";
-                  rows+= "</tbody>";
-                }
-                table1.innerHTML=rows;
-                let imgDataArray=["LEAD & VOC Pack", "Chloramine & VOC Pack","Anti-Scale & Heavy Metals Pack","LEAD & VOC Pack", "Chloramine & VOC Pack","Chloramine & VOC Pack","Anti-Scale & Heavy Metals Pack",]
-                let uniqueImgArr=[...new Set(imgDataArray)]
-                let images= {'LEAD & VOC Pack':'Images/Lead&VOC.png',
-                            'Chloramine & VOC Pack':'Images/Chloramine.png',
-                            'Anti-Scale & Heavy Metals Pack':'Images/AntiScale.png'
-                }
-                imgDiv.innerHTML="";
-uniqueImgArr.forEach((code)=>{
-   let img=document.createElement('img');
-   img.src=images[code];
-   img.classList.add('img-fluid');
-   img.classList.add('mx-2');
-   img.style.height='80px';
-   imgDiv.appendChild(img)
-})
+                // Iterate over the keys and values of the downloadLinks object
+                Object.entries(data.downloadLinks).forEach(([fileName, filePath], index) => {
+                    // Remove the base path from the URL
+                    const cleanedUrl = filePath.replace(basePathToRemove, '');
+            
+                    linksHTML += `
+                        <div class="mb-3">
+                            <a href="${cleanedUrl}" target="_blank">Download ${fileName}</a>
+                            <br>
+                            <small>URL: http://82.180.137.104:3000${cleanedUrl}</small>
+                        </div>
+                    `;
+                });
+            
+                doneMessage.innerHTML = linksHTML;
+            } else {
+                doneMessage.textContent = 'Processing completed, but no download links were provided.';
+            }
+
+        })
+        .catch(error => {
+            processingMessage.style.display = 'block';
+            processingMessage.textContent = `Error: ${error.message}`;
+        })
+        .finally(() => {
+            processingMessage.style.display = 'none';
+            doneMessage.style.display = 'block';
+        });
+});
 
 
-              let checkBoxVal=flexCheckDefault.checked;
-              console.log("check box value   " +checkBoxVal);
+// Functions
+function printVal() {
+    const zipCodeVal = zipCode.value;
 
-             
-              if(checkBoxVal){
-                let data1= [["Shh-125","Lorem  Account" ],
-                ["DD-125","Cara grteon"],
-                ["DD-125","Cara grteon"],
-                ["DD-125","Cara grteon"]]
+    if (zipCodeVal === "123") {
+        const data = [
+            ['PB90', 'LEAD SUMMARY', 'Lead is a toxic heavy metal...', 'Lead exposure is highly dangerous...', 'LEAD & VOC Pack'],
+            ['CU90', 'COPPER SUMMARY', 'Copper is a metal...', 'Excessive exposure can cause...', 'Anti-Scale & Heavy Metals Pack'],
+            ['2V08', '8 Regulated Phase I VOCs', '1. Benzene\n2. Carbon Tetrachloride\n...', 'This group of contaminants...', 'Chloramine & VOC Pack'],
+            ['2V07', '7 Regulated Phase I VOCs', '1. Benzene\n2. Carbon Tetrachloride\n...', 'This group of contaminants...', 'Chloramine & VOC Pack']
+        ];
+
+        if (data.length > 0) {
+            table1Con.classList.remove("d-none");
+            hrTag.classList.remove("d-none");
+
+            table1.innerHTML = generateTable(data, ["Code", "Description", "Contaminant Description", "Health Hazard", "Filter Pack Match"]);
+
+            const imgDataArray = data.map(item => item[4]);
+            const uniqueImgArr = [...new Set(imgDataArray)];
+            const images = {
+                'LEAD & VOC Pack': 'Images/Lead&VOC.png',
+                'Chloramine & VOC Pack': 'Images/Chloramine.png',
+                'Anti-Scale & Heavy Metals Pack': 'Images/AntiScale.png'
+            };
+
+            imgDiv.innerHTML = uniqueImgArr.map(code => `<img src="${images[code]}" class="img-fluid mx-2" style="height: 80px;">`).join('');
+
+            if (flexCheckDefault.checked) {
+                const data1 = [
+                    ["Shh-125", "Lorem Account"],
+                    ["DD-125", "Cara grteon"]
+                ];
                 table2Vio.classList.remove("d-none");
-                let rows1=""
-               
-                rows1+= "<thead>";
-                rows1+= "<tr>";
-                rows1+= "<th style='width:100px;'>S.No.</th>";
-                rows1+= "<th>Description</th>";
-                rows1+= "</tr>";
-                rows1+= "</thead>";
-                for(var i=0; i<data1.length; i++){
-                  rows1+= "<tbody>";
-                  rows1+= "<tr>";
-                  rows1+= "<td>"+ data1[i][0] +"</td>";
-                  rows1+= "<td>"+ data1[i][1] +"</td>";
-                  rows1+= "</tr>";
-                  rows1+= "</tbody>";
-                }
-                table2.innerHTML=rows1;
-
-              }
+                table2.innerHTML = generateTable(data1, ["S.No.", "Description"]);
+            }
         }
 
-    }else{
-        imgDiv.innerHTML="";
-        table1.innerHTML="";
-        table2.innerHTML="";
-        table1Con.classList.add("d-none");
-        table2Vio.classList.add("d-none");  
-        hrTag.classList.add("d-none");
-        errorMsg.innerHTML= "Enter a valid Zip Code";
-        
-        setTimeout(function () {document.getElementById("errorMsg").innerHTML = "";
-            
-        }, 3000);
+    } else {
+        clearTables();
+        showError("Enter a valid Zip Code");
     }
-   
 }
 
+function generateTable(data, headers) {
+    const headerRow = headers.map(header => `<th>${header}</th>`).join('');
+    const rows = data.map(row => `<tr>${row.map(cell => `<td>${cell}</td>`).join('')}</tr>`).join('');
+    return `<thead><tr>${headerRow}</tr></thead><tbody>${rows}</tbody>`;
+}
 
-  
+function clearTables() {
+    imgDiv.innerHTML = "";
+    table1.innerHTML = "";
+    table2.innerHTML = "";
+    table1Con.classList.add("d-none");
+    table2Vio.classList.add("d-none");
+    hrTag.classList.add("d-none");
+}
 
+function showError(message) {
+    errorMsg.innerText = message;
+    setTimeout(() => { errorMsg.innerText = ""; }, 3000);
+}
 
-function adminAccess(){
-    
+function adminAccess() {
     inputAdmin.classList.remove("d-none");
     btnAdmin.classList.remove("d-none");
-    // adminAccessBtn.classList.remove("btn-primary");
-    // adminAccessBtn.classList.add("btn-secondary");
     adminAccessBtn.disabled = true;
 }
 
-function submitPassword(){
+function submitPassword() {
+    const originalPass = "12345";
+    const enteredPass = inputAdmin.value;
 
-    let originalPass= "12345"
-    let enteredPass = inputAdmin.value;
-if(originalPass == enteredPass){
-    inputAdmin.classList.add("d-none");
-    btnAdmin.classList.add("d-none");
-    uploadBox.classList.remove("d-none");
-    uploadData.classList.remove("d-none");
-    downloadData.classList.remove("d-none");
-    
-}else{
-    
-pTag.innerText= "You have entered wrong Password!"
-setTimeout(function () {document.getElementById("pTag").innerHTML = ""}, 3000);
-
-}
-}
-
-
-function unique(){
-    let mainArray=["LEAD & VOC Pack", "Chloramine & VOC Pack","Anti-Scale & Heavy Metals Pack","LEAD & VOC Pack", "Chloramine & VOC Pack","Chloramine & VOC Pack","Anti-Scale & Heavy Metals Pack",]
-    let uniqueArr=[...new Set(mainArray)]
-    console.log(uniqueArr)
+    if (originalPass === enteredPass) {
+        inputAdmin.classList.add("d-none");
+        btnAdmin.classList.add("d-none");
+        uploadBox.classList.remove("d-none");
+        uploadData.classList.remove("d-none");
+        downloadData.classList.remove("d-none");
+    } else {
+        pTag.innerText = "You have entered the wrong Password!";
+        setTimeout(() => { pTag.innerText = ""; }, 3000);
     }
-
-function uploadContaminationData(){
-
-}
-
-function zipCodeSearch(){
-console.log("Hello")
 }
---
+++ server.js
@@ -0,0 +1,252 @@
+const express = require('express');
+const axios = require('axios');
+const unzipper = require('unzipper');
+const csv = require('csv-parser');
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
+const { pipeline } = require('stream/promises');
+const { Readable, Writable } = require('stream');
+
+const app = express();
+const port = 3000;
+
+const fileUrl = 'https://echo.epa.gov/files/echodownloads/SDWA_latest_downloads.zip';
+const downloadDir = path.join(__dirname, 'downloads');
+const zipcodeFilePath = path.join(downloadDir, 'zipcodes.csv');
+
+// Serve the styles.css file
+app.get('/styles.css', (req, res) => {
+    res.sendFile(path.join(__dirname, 'styles.css'), {
+        headers: {
+            'Content-Type': 'text/css'
+        }
+    });
+});
+
+app.use(express.static(path.join(__dirname)));
+
+let processRunning = false;
+let processAbortController = null;
+
+// Utility function to clear the downloads directory except the zipcode file
+async function clearDownloadsDirectory() {
+  try {
+    const files = await fs.promises.readdir(downloadDir);
+    await Promise.all(
+      files
+        .filter(file => file !== 'zipcodes.csv')
+        .map(file => fs.promises.rm(path.join(downloadDir, file), { recursive: true, force: true }))
+    );
+  } catch (error) {
+    console.error('Error clearing the downloads directory:', error);
+  }
+}
+// Function to load zipcode data into memory and create a map
+async function loadZipcodeData() {
+  const zipcodeMap = new Map();
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(zipcodeFilePath)
+      .pipe(csv())
+      .on('data', (row) => {
+        const zipCity = row['CityName'] ? row['CityName'].toLowerCase() : '';
+        const zipState = row['StateAbbr'] || '';
+        const zipCode = row['ZIPCode'] || '';  
+        const key = `${zipState}-${zipCity}`;
+        zipcodeMap.set(key, zipCode);
+      })
+      .on('end', () => resolve(zipcodeMap))
+      .on('error', (err) => reject(err));
+  });
+}
+
+// Function to find a matching zipcode from the map
+function findMatchingZipcode(zipcodeMap, stateServed, cityServed) {
+  if (stateServed && cityServed) {
+    const key = `${stateServed}-${cityServed.toLowerCase()}`;
+    return zipcodeMap.get(key);
+  }
+  return undefined;
+}
+
+async function processCSV(entry, fileName, selectedFields, addZipcode = false, pwsidZipcodeMap = null, zipcodeMap = null) {
+  const outputFilePath = path.join(downloadDir, fileName); //.replace('.csv', '_filtered.csv')
+  const outputStream = fs.createWriteStream(outputFilePath);
+
+  console.log('Processing file : ', fileName)
+
+  // Write header to the output stream
+  outputStream.write(selectedFields.join(',') + ',ZIPCODE' + '\n');
+
+  const transformStream = new Writable({
+    objectMode: true,
+    write(row, encoding, callback) {
+      let outputRow = selectedFields.map(field => row[field] || '');
+      if (addZipcode) {
+        const stateServed = row['STATE_SERVED'];
+        const pwsid = row['PWSID'];
+        const cityServed = row['CITY_SERVED'];
+        if (stateServed && cityServed) {
+          const matchingZip = findMatchingZipcode(zipcodeMap, stateServed, cityServed);
+          if (matchingZip) {
+            outputRow.push(matchingZip);
+            pwsidZipcodeMap.set(pwsid, matchingZip);
+          } else {
+            return callback(); // Skip row if no matching zipcode
+          }
+        } else {
+          return callback(); // Skip row if STATE_SERVED or CITY_SERVED is blank
+        }
+      }
+      if ( (fileName === 'SDWA_LCR_SAMPLES.csv' || fileName === 'SDWA_VIOLATIONS_ENFORCEMENT.csv' ) && pwsidZipcodeMap) {
+        const pwsid = row['PWSID'];
+        const zipcode = pwsidZipcodeMap.get(pwsid) || '';
+        if (zipcode) {
+          outputRow.push(zipcode);
+          outputStream.write(outputRow.join(',') + '\n');
+        }
+      } else if (!addZipcode || fileName !== 'SDWA_LCR_SAMPLES.csv') {
+        if (addZipcode && !outputRow[outputRow.length - 1]) {
+          return callback(); // Skip row if ZIPCODE field is blank
+        }
+        outputStream.write(outputRow.join(',') + '\n');
+      }
+      callback();
+    }
+  });
+
+  try {
+    await pipeline(
+      entry,
+      csv(),
+      transformStream
+    );
+    outputStream.end();
+    console.log(`Processed CSV file: ${fileName}`);
+    return outputFilePath;
+  } catch (err) {
+    console.error('Error processing CSV file:', err);
+    throw err;
+  }
+}
+
+async function downloadAndProcessCSVs(url) {
+  try {
+    const response = await axios({
+      method: 'GET',
+      url: url,
+      responseType: 'stream',
+      timeout: 1200000,
+      signal: processAbortController.signal // handle abort
+    });
+
+    const zipcodeMap = await loadZipcodeData();
+    const pwsidZipcodeMap = new Map();
+    
+    const filePaths = {};
+    const processFiles = [
+      { name: 'SDWA_GEOGRAPHIC_AREAS.csv', fields: ['PWSID', 'STATE_SERVED', 'CITY_SERVED'], addZipcode: true },
+      { name: 'SDWA_LCR_SAMPLES.csv', fields: ['PWSID', 'SAMPLE_LAST_REPORTED_DATE', 'CONTAMINANT_CODE', 'RESULT_SIGN_CODE', 'SAMPLE_MEASURE', 'UNIT_OF_MEASURE'] },
+      { name: 'SDWA_VIOLATIONS_ENFORCEMENT.csv', fields: ['PWSID', 'VIOLATION_CATEGORY_CODE', 'CONTAMINANT_CODE', 'VIOLATION_STATUS','VIOL_LAST_REPORTED_DATE'] }
+    ];
+
+    let processedCount = 0;
+
+    return new Promise((resolve, reject) => {
+      const parseStream = response.data.pipe(unzipper.Parse());
+
+      parseStream.on('entry', async (entry) => {
+        const fileName = entry.path;
+        const file = processFiles.find(f => f.name === fileName);
+
+        if (file) {
+          try {
+            //file.name.replace('.csv', '_filtered.csv')
+            filePaths[file.name] = await processCSV(entry, file.name, file.fields, file.addZipcode, pwsidZipcodeMap, zipcodeMap);
+            processedCount++;
+            if (processedCount === processFiles.length) {
+              parseStream.emit('end'); // Manually emit 'end' event
+            }
+          } catch (err) {
+            parseStream.emit('error', err); // Propagate error to the main stream
+          }
+        } else {
+          entry.autodrain(); // Skip files not in the processFiles list
+        }
+      });
+
+      parseStream.on('end', () => {
+        resolve(filePaths);
+      });
+
+      parseStream.on('error', (err) => {
+        console.error('Error with unzipper stream:', err);
+        reject(err);
+      });
+    });
+  } catch (err) {
+    console.error('Error downloading and processing CSVs:', err);
+    throw err;
+  }
+}
+
+app.get('/process-data', async (req, res) => {
+  if (processRunning) {
+    return res.status(400).json({ message: 'Process is already running' });
+  }
+
+  processRunning = true;
+  processAbortController = new AbortController();
+
+  const startTime = new Date();
+
+  try {
+
+    await clearDownloadsDirectory();
+    const filePaths = await downloadAndProcessCSVs(fileUrl);
+
+    //Don't need to store 'SDWA_GEOGRAPHIC_AREAS.csv', so delete it from 
+    if (filePaths['SDWA_GEOGRAPHIC_AREAS.csv']) {
+      delete filePaths['SDWA_GEOGRAPHIC_AREAS.csv'];
+    }
+
+    // delete file, which don't need
+    const files = await fs.promises.readdir(downloadDir);
+    const fileToDelete = 'SDWA_GEOGRAPHIC_AREAS.csv';
+    if (files.includes(fileToDelete)) {
+      await fs.promises.rm(path.join(downloadDir, fileToDelete), { recursive: true, force: true });
+    }
+    
+    console.log(filePaths)
+
+    //Upload files to Google Cloud Storage
+    // const bucketName = process.env.BUCKET_NAME;
+    // const uploadPromises = Object.entries(filePaths).map(async ([key, filePath]) => {
+    //   const mediaLink = await uploadFile(bucketName, filePath, key);
+    //   return { [key]: mediaLink };
+    // });
+
+    // const uploadedFiles = await Promise.all(uploadPromises);
+
+    const endTime = new Date();
+    const timeTaken = endTime - startTime;
+    console.log(`CSV processing completed in ${timeTaken} ms`);
+
+    res.json({ downloadLinks : filePaths}); //uploadedFiles
+  } catch (error) {
+    console.error('Error processing files:', error);
+    res.status(500).send('Internal Server Error');
+  } finally {
+    processRunning = false;
+    processAbortController = null;
+  }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
